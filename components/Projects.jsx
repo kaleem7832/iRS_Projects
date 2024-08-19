429e2d1b1fea5febcdb0e54bf9910c234a2be293
Kaleem Nalband <kaleem.nalband@gmail.com>
2024-08-19 15:21:10 +0530

    no cache added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,7 +8,7 @@ export default function Projects() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch("api/projects");
+        const response = await fetch("api/projects", { cache: "no-store" });
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }

51679a4d231b8e29d19d6a2a06c0321851fa3ffb
Kaleem Nalband <kaleem.nalband@gmail.com>
2024-11-04 16:14:21 +0530

    loading - update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
+import Loading from "./loading";
+
 import Projects from "@/components/Projects";
 
 export default function Dashboard() {
-   
-  return (<>
-    <Projects/>
-  </>);
+  return (
+    <Suspense fallback={<Loading />}>
+      <Projects />
+    </Suspense>
+  );
 }
